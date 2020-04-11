5a27e5ef46e7619f1bf49bb79d48e00d6eb23c77
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2020-04-11 14:39:06 +0530

    docs update

---
+++ docs/precache-manifest.b9d24d77fed7ab7bb74d6802c597f809.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ef511ad1750292c71fc3209577dde5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "581020755486f63e35d9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e0abd856b128393458e8",
+    "url": "/static/js/2.4863e047.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4863e047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581020755486f63e35d9",
+    "url": "/static/js/main.7ae10f89.chunk.js"
+  },
+  {
+    "revision": "4d6209d3bef6bff99bda",
+    "url": "/static/js/runtime-main.82a0fb78.js"
+  },
+  {
+    "revision": "a0a6971ce3099ac7073e5c7c9ee910a6",
+    "url": "/static/media/science (3).a0a6971c.png"
+  }
+]);
